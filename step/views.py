00b01e623261b8ad160649b8ab5568d3e1d38b94
njnisarg <NJnisarg@gmail.com>
2019-03-10 00:25:36 +0530

    Added a gitignore file. Fixed the issue with image not rendering in info.html.

--- step/views.py
+++ step/views.py
@@ -1,30 +1,32 @@
-from django.http import HttpResponse,HttpResponseRedirect
-from django.urls import reverse
-import datetime
-from django import template
-from django.utils import timezone
-
-from django.shortcuts import render,get_object_or_404
-from .models import govtProjects,ongoingProgrammes,comingEvents
-
-from django.urls import reverse
-
+# from django.http import HttpResponse,HttpResponseRedirect
+# from django.urls import reverse
+# import datetime
+# from django import template
+# from django.utils import timezone
+# from django.urls import reverse
+from django.shortcuts import render
+from .models import govtProjects, ongoingProgrammes, comingEvents
 
 
 def frontPage(request):
- return render(request,"step/front-page.html") 
+    return render(request, "step/front-page.html")
+
 
 def gov_projects(request):
-    obj=govtProjects.objects.all()
-    context={'obj':obj,}
-    return render(request,'step/info.html',context)
+    obj = govtProjects.objects.all()
+    context = {'obj': obj}
+    return render(request, 'step/info.html', context)
+
+
 def ongoing(request):
-    obj=ongoingProgrammes.objects.all()
-    context={'obj':obj,}
-    return render(request,'step/info.html',context)
+    obj = ongoingProgrammes.objects.all()
+    context = {'obj': obj}
+    return render(request, 'step/info.html', context)
+
+
 def comingevents(request):
-    obj=comingEvents.objects.all()
-    context={'obj':obj,}
-    return render(request,'step/info.html',context)
+    obj = comingEvents.objects.all()
+    context = {'obj': obj}
+    return render(request, 'step/info.html', context)
 
 
