712ae8b89f752eecd267ee63608c3174257ec944
Vasudev B M <mananpoddarm@gmail.com>
2019-03-09 11:04:03 +0530

    database created

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     url(r'^step/', include("step.urls" , namespace="step")),
 
 ]
--- step/models.py
+++ step/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class govtProjects(models.Model):
+    image = models.ImageField(upload_to='documents/')
+    title = models.CharField(max_length = 80, blank =  False  )
+    description = models.TextField(blank =  False  )
+    details = models.TextField(blank =  False  )
+
+
+
+class ongoingProgrammes(models.Model):
+    image = models.ImageField(upload_to='documents/')
+    title = models.CharField(max_length = 80, blank =  False  )
+    description = models.TextField(blank =  False  )
+    details = models.TextField(blank =  False  )
+
+
+class comingEvents(models.Model):
+    image = models.ImageField(upload_to='documents/')
+    title = models.CharField(max_length = 80, blank =  False  )
+    description = models.TextField(blank =  False  )
+    details = models.TextField(blank =  False  )
